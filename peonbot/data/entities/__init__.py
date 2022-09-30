7655eff173ed533000197c4aef58872b2ec605bc
saweima12 <saweima12@gmail.com>
2022-09-30 16:31:51 +0800

    declare entities model

---
+++ peonbot/data/entities/__init__.py
@@ -0,0 +1,6 @@
+from .chat_behavior_record import ChatBehaviorRecord
+from .chat_config import ChatConfig
+from .chat_deleted_message import ChatDeletedMessage
+from .chat_saved_message import ChatSavedMessage
+from .chat_user_blacklist import ChatUrlBlackList
+from .chat_watch_user import ChatWatchUser
---
+++ peonbot/data/entities/chat_deleted_message.py
@@ -0,0 +1,12 @@
+from tortoise import fields
+from tortoise.models import Model
+
+class ChatDeletedMessage(Model):
+
+    chat_id = fields.CharField(max_length=40, index=True)
+    content_type = fields.CharField(max_length=40, index=True)
+    message_json = fields.JSONField()
+    record_date = fields.DatetimeField(auto_now_add=True)
+
+    class Meta:
+        table = "peon_deleted_message"
