aa031f45bccbb88ad52b8ca14bcdb3719c6be4ef
saweima12 <saweima12@gmail.com>
2022-10-03 10:21:11 +0800

    adjust shell command

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export INLINEBOT_CONFIG="$(pwd)/env.py"
-pipenv run sanic inlinebot:app -H 0.0.0.0 -p 8000 -d
+export BOT_CONFIG="$(pwd)/env.py"
+pipenv run sanic peonbot:app -H 0.0.0.0 -p 8000 -d
