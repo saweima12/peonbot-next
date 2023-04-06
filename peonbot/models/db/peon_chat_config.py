456dae5f95b88b6ad4837bccfa4a7a64072d04f3
saweima12 <saweima12@gmail.com>
2023-04-07 01:14:32 +0800

    Add tortoise orm model.

---
+++ peonbot/models/db/peon_chat_config.py
@@ -0,0 +1,15 @@
+from tortoise import fields
+from tortoise.models import Model
+
+
+class PeonChatConfig(Model):
+
+    chat_id = fields.CharField(max_length=40, index=True)
+    status = fields.CharField(max_length=8, index=True)
+    chat_name = fields.TextField()
+    config_json = fields.JSONField()
+    permission_json = fields.JSONField(default={})
+    attach_json = fields.JSONField(default={})
+
+    class Meta:
+        table = "peon_chat_config"
